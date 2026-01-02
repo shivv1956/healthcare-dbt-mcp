3f37d15ceaab7c76d46d97c06a1252e99ff0c2c3
chethanr <chethanr@phdata.io>
2026-01-02 15:46:13 +0530

    docs: add descriptions to column tests in staging models

--- Silver/models/staging/stg_organizations.sql
+++ Silver/models/staging/stg_organizations.sql
@@ -21,26 +21,25 @@ with source as (
 validated as (
 
     select
-        {{ validate_uuid('Id') }} as organization_id,
+        "Id" as organization_id,
 
-        Name    as name,
-        Address as address,
-        City    as city,
-        State   as state,
-        Zip     as zip,
+        "Name"    as name,
+        "Address" as address,
+        "City"    as city,
+        "State"   as state,
+        "Zip"     as zip,
 
-        Lat::number(18,6) as latitude,
-        Lon::number(18,6) as longitude,
-
-        Phone as phone,
-        Revenue::number(18,2) as revenue,
-        Utilization::number(38,0) as utilization,
+        "Lat"::number(18,6) as latitude,
+        "Lon"::number(18,6) as longitude,
+        "Phone" as phone,
+        "Revenue"::number(18,2) as revenue,
+        "Utilization"::number(38,0) as utilization,
 
         SOURCE_FILE_NAME,
         LOAD_TIMESTAMP,
 
         row_number() over (
-            partition by {{ validate_uuid('Id') }}
+            partition by "Id"
             order by LOAD_TIMESTAMP desc
         ) as rn
 
@@ -51,8 +50,4 @@ select *
 from validated
 where rn = 1
   and organization_id is not null
-  and name is not null
-  and address is not null
-  and city is not null
-  and revenue is not null
-  and utilization is not null;
+  
